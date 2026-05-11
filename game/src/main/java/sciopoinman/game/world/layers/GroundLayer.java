96bcdc64aecd87945ae981be01e1edcdf889d518
TeoIce <matteo.saramin@istitutovolterra.edu.it>
2026-05-11 17:24:42 +0200

    alphamap and heightmap change

--- game/src/main/java/sciopoinman/game/world/layers/GroundLayer.java
+++ game/src/main/java/sciopoinman/game/world/layers/GroundLayer.java
@@ -11,24 +11,25 @@ import com.jme3.terrain.geomipmap.TerrainQuad;
 import com.jme3.terrain.geomipmap.lodcalc.DistanceLodCalculator;
 import com.jme3.terrain.heightmap.AbstractHeightMap;
 import com.jme3.terrain.heightmap.ImageBasedHeightMap;
+
 import com.jme3.texture.Texture;
 
 public class GroundLayer {
     private TerrainQuad terrain;
     private Material matRock;
-    private float grassScale = 64;
-    private float dirtScale = 16;
+    private float grassScale = 1024;
+    private float dirtScale = 32;
     private float rockScale = 128;
 
     public GroundLayer(AssetManager assetManager, Node rootNode, PhysicsSpace physicsSpace, Camera cam) {
 
-        Texture heightMapImage = assetManager.loadTexture("Textures/Terrain/splat/mountains1024.jpg");
+        Texture heightMapImage = assetManager.loadTexture("Textures/Terrain/splat/HeightMapTrenches.png");
         matRock = new Material(assetManager, "Common/MatDefs/Terrain/Terrain.j3md");
         matRock.setBoolean("useTriPlanarMapping", false);
-        matRock.setTexture("Alpha", assetManager.loadTexture("Textures/Terrain/splat/alphamap.png"));
+        matRock.setTexture("Alpha", assetManager.loadTexture("Textures/Terrain/splat/alphamapTrenches.png"));
 
         // GRASS texture
-        Texture grass = assetManager.loadTexture("Textures/Terrain/splat/grass.jpg");
+        Texture grass = assetManager.loadTexture("Textures/Terrain/splat/fango.png");
         grass.setWrap(Texture.WrapMode.Repeat);
         matRock.setTexture("Tex1", grass);
         matRock.setFloat("Tex1Scale", grassScale);
@@ -48,25 +49,24 @@ public class GroundLayer {
         AbstractHeightMap heightmap = null;
         try {
             // heightmap = new HillHeightMap(513, 600, 10, 40, (byte) 3);
-            heightmap = new ImageBasedHeightMap(heightMapImage.getImage(), 1f);
+            heightmap = new ImageBasedHeightMap(heightMapImage.getImage(), 120f);
             heightmap.load();
             heightmap.smooth(0.9f, 2);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        terrain = new TerrainQuad("terrain", 65, 513, heightmap.getHeightMap());
+        terrain = new TerrainQuad("terrain", 65, 1025, heightmap.getHeightMap());
         TerrainLodControl control = new TerrainLodControl(terrain, cam);
         control.setLodCalculator( new DistanceLodCalculator(65, 2.7f) ); // patch size, and a multiplier
         terrain.addControl(control);
         terrain.setMaterial(matRock);
-        terrain.setLocalTranslation(-50f, -5, 50f);
-        terrain.setLocalScale(1f, 0.05f, 1f);
-
-        rootNode.attachChild(terrain);
+        terrain.setLocalTranslation(-50f, -300f, 50f);
+        terrain.setLocalScale(0.6f, 0.01f, 0.6f);
 
         RigidBodyControl groundPhysics = new RigidBodyControl(0f);
         terrain.addControl(groundPhysics);
+        rootNode.attachChild(terrain);
         physicsSpace.add(groundPhysics);
     }
 }
